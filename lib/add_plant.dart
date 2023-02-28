8a544b579af9a52e618b896e1254196935e9db75
Muhammad Mannan <usmanmannan8@gmail.com>
2023-02-27 21:11:46 -0800

    ui updates

--- lib/add_plant.dart
+++ lib/add_plant.dart
@@ -32,9 +32,36 @@ class _MyAddPlant extends State<AddPlant> {
   void initState() {
     super.initState();
     _date = DateTime.now();
+    _getCurrentLocation();
   }
 
-  Future<void> _getCurrentLocation() async {
+  Future<Position> _getCurrentLocation() async {
+    LocationPermission permission = await Geolocator.checkPermission();
+
+    if (permission == LocationPermission.denied) {
+      permission = await Geolocator.requestPermission();
+      if (permission == LocationPermission.denied) {
+        return Future.error('Location permission denied');
+      }
+    }
+
+    if (permission == LocationPermission.whileInUse ||
+        permission == LocationPermission.always) {
+      // Permission has been granted, get current location
+      try {
+        final position = await Geolocator.getCurrentPosition();
+        return position;
+      } catch (e) {
+        return Future.error('Error getting current position: $e');
+      }
+    }
+
+    // Permission is still being requested, wait for a short delay and try again
+    await Future.delayed(Duration(milliseconds: 500));
+    return _getCurrentLocation();
+  }
+
+  /* Future<void> _getCurrentLocation() async {
     final position = await _determinePosition();
     setState(() {
       _latitude = position.latitude.toDouble();
@@ -55,7 +82,7 @@ class _MyAddPlant extends State<AddPlant> {
     }
 
     return await Geolocator.getCurrentPosition();
-  }
+  } */
 
   @override
   void dispose() {
@@ -102,7 +129,48 @@ class _MyAddPlant extends State<AddPlant> {
                   return null;
                 },
               ),
-              Center(
+              FutureBuilder<Position>(
+                future: _getCurrentLocation(),
+                builder: (context, snapshot) {
+                  if (snapshot.hasData) {
+                    final Position position = snapshot.data!;
+                    return Stack(
+                      children: [
+                        Container(
+                          color: Colors.black.withOpacity(0.5),
+                        ),
+                        Column(
+                          children: [
+                            Center(
+                                child:
+                                    Text('\nLatitude: ${position.latitude}')),
+                            Center(
+                                child:
+                                    Text('Longitude: ${position.longitude}')),
+                          ],
+                        ),
+                      ],
+                    );
+                  } else if (snapshot.hasError) {
+                    return Center(
+                      child: Text('${snapshot.error}'),
+                    );
+                  } else {
+                    return Stack(
+                      children: [
+                        Container(
+                          color: Colors.black.withOpacity(0.5),
+                        ),
+                        Center(
+                          child: CircularProgressIndicator(),
+                        ),
+                      ],
+                    );
+                  }
+                },
+              ),
+
+              /* Center(
                 child: Padding(
                   padding: EdgeInsets.symmetric(vertical: 16.0),
                   child: ElevatedButton(
@@ -114,9 +182,9 @@ class _MyAddPlant extends State<AddPlant> {
                     child: Text('Get Location'),
                   ),
                 ),
-              ),
-              Center(child: Text('Latitude: $_latitude')),
-              Center(child: Text('Longitude: $_longitude')),
+              ), */
+              /* Center(child: Text('Latitude: $_latitude')),
+              Center(child: Text('Longitude: $_longitude')), */
               Center(
                 child: Padding(
                   padding: EdgeInsets.symmetric(vertical: 16.0),
